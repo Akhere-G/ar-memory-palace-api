1791673081757e796398239e05a1778d692d6db9
Akhere Ihoeghinlan <101akhere5@gmail.com>
2021-12-13 11:41:16 +0000

    initial server layout

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/", (req, res) => res.send("Welcone to the memory palace api!"));
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at port ${PORT}`)))
+  .catch((err) => console.log(`Could not connect. ${err.message}`));
