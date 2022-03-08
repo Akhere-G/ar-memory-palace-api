f647922a5fa7a532bfe950dbd6cddbf2ae23213f
Akhere Ihoeghinlan <101akhere5@gmail.com>
2022-03-08 14:44:59 +0000

    fixed failed to connect bug

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => 
-  .catch((err) => 
+  .then(() => app.listen(PORT, () => console.log(`Listening at port ${PORT}`)))
+  .catch((err) => console.log(`Could not connect. ${err.message}`));
