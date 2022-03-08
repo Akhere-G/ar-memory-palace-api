b3832cbde898eab3ea52fae38807f33b0a2ad2c7
Akhere Ihoeghinlan <101akhere5@gmail.com>
2022-03-08 15:20:30 +0000

    Added more tests for signing into a group

--- __tests__/signIntoGroup.test.js
+++ __tests__/signIntoGroup.test.js
@@ -13,9 +13,8 @@ describe("api/groups", () => {
       jwt.sign = () => "token";
     });
 
-    it("fails when group doesn't exist", async () => {
+    it("sends an error message that name is missing", async () => {
       const groupData = {
-        named: "name",
         password: "password",
       };
 
@@ -29,6 +28,65 @@ describe("api/groups", () => {
       expect(response.statusCode).toBe(400);
       expect(response.body).toStrictEqual({
         message: "name and password are required",
+      });
+    });
+
+    it("sends an error message thatpassword is missing", async () => {
+      const groupData = {
+        name: "name",
+      };
+
+      Group.findOne = jest.fn(() => null);
+
+      const response = await request(app)
+        .post("/api/groups/signin")
+        .send(groupData)
+        .expect("Content-Type", /json/);
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toStrictEqual({
+        message: "name and password are required",
+      });
+    });
+
+    it("fails when group doesn't exist", async () => {
+      const groupData = {
+        name: "name",
+        password: "password",
+      };
+
+      Group.findOne = jest.fn(() => null);
+
+      const response = await request(app)
+        .post("/api/groups/signin")
+        .send(groupData)
+        .expect("Content-Type", /json/);
+
+      expect(response.statusCode).toBe(404);
+      expect(response.body).toStrictEqual({
+        message: "group doesn't exist",
+      });
+    });
+
+    it("sends an error message when credentials are invalid", async () => {
+      const groupData = {
+        name: "name",
+        password: "password",
+      };
+
+      Group.findOne = jest.fn(() => ({
+        name: "name",
+        password: "hash_of_different_group",
+      }));
+
+      const response = await request(app)
+        .post("/api/groups/signin")
+        .send(groupData)
+        .expect("Content-Type", /json/);
+
+      expect(response.statusCode).toBe(400);
+      expect(response.body).toStrictEqual({
+        message: "Invalid credentials",
       });
     });
   });
